a72666581de4dafa5b9afc26aa74cf82ac45c001
BhargaviSheth <it.sheth.bhargavi@gmail.com>
2024-05-08 05:50:56 +0530

    take input & console

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 console.log("hello the script is working")
-var username = prompt("give username")
-alert("this sctipt works - alert" + username)
+
+var btnTranslate = document.querySelector("#btn-translate")
+var txtinput = document.querySelector("#txt-input")
+
+//console.log(txtinput)
+
+
+btnTranslate.addEventListener('click',function clickbtn(){
+  console.log("input" , txtinput.value)
+  //console.log("you clicked button")
+})
